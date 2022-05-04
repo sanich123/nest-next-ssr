f5a7d064e607eb4d5114e199826ff0f37407d03f
Alexander Voronin <aovoronin.piano@gmail.com>
2022-05-04 17:05:23 +0300

    Some init commit

---
+++ src/app.controller.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+@Controller('/api')
+export class AppController {
+    constructor(private appService: AppService) {
+
+    }
+  @Get()
+  getUsers() {
+    return this.appService.getUsers();
+  }
+}
