e2fcf1f9f7e6a19211ad44914c89b02cfe25fd4a
Alexander Voronin <aovoronin.piano@gmail.com>
2022-05-04 20:52:58 +0300

    Done some work with services, modules

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { CreateTrackDto } from './dto/create-track-dto';
 import { Comment, CommentDocument } from './schemas/comment.schema';
 import { Track, TrackDocument } from './schemas/track.schema';
 
@@ -10,7 +11,10 @@ export class TrackService {
     @InjectModel(Track.name) private trackModel: Model<TrackDocument>,
     @InjectModel(Comment.name) private commentModel: Model<CommentDocument>,
   ) {}
-  async create() {}
+  async create(dto: CreateTrackDto): Promise<Track> {
+    const track = await this.trackModel.create({ ...dto, listens: 0 })
+    return track;
+  }
 
   async getAll() {}
 
